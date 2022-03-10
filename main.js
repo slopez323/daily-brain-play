8aa6448bd7938d07220ea874106c6b4354bfe63e
Sarah Lopez <slopez323@gmail.com>
2022-03-10 18:01:43 -0500

    draft 2

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
-// if ($(window).width() < 450){
-//     $('.largescreen').hide();
-// } else {
-//     $('.smallscreen').hide();
-// }
+document.addEventListener("touchstart", function () { }, false);
 
-// $(window).resize(function(){
-// if ($(window).width() < 450){
-//     $('.largescreen').hide();
-//     $('.smallscreen').show();
-// } else {
-//     $('.smallscreen').hide();
-//     $('.largescreen').show();
-// }
-// })
+$('.getphrazy').click(function(){
+    window.location = 'https://getphrazy.com/'
+})
+
+$('.word').click(function(){
+    window.location = 'https://word.dailybrainplay.com/'
+})
