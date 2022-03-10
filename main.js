34b27a9cd714c39aa09baeef476859a249d58e61
Sarah Lopez <slopez323@gmail.com>
2022-03-10 17:37:21 -0500

    first draft

---
+++ main.js
@@ -0,0 +1,15 @@
+// if ($(window).width() < 450){
+//     $('.largescreen').hide();
+// } else {
+//     $('.smallscreen').hide();
+// }
+
+// $(window).resize(function(){
+// if ($(window).width() < 450){
+//     $('.largescreen').hide();
+//     $('.smallscreen').show();
+// } else {
+//     $('.smallscreen').hide();
+//     $('.largescreen').show();
+// }
+// })
