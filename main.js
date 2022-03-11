bc9bbe446ad52f5ecda280e3b26ec50ae57233af
Sarah Lopez <slopez323@gmail.com>
2022-03-11 16:16:14 -0500

    mobile play button

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 document.addEventListener("touchstart", function () { }, false);
 
 function isTouchScreendevice() {
-    return 'ontouchstart' in window || navigator.maxTouchPoints;      
+    return 'ontouchstart' in window || navigator.maxTouchPoints;
 };
 
 $('.getphrazy').click(function () {
-    if(!isTouchScreendevice()) {
+    if (!isTouchScreendevice()) {
         window.location = 'https://getphrazy.com/'
     } else {
-
+        $('.getphrazy-play').show();
     }
 })
 
 $('.word').click(function () {
-    if(!isTouchScreendevice()) {
+    if (!isTouchScreendevice()) {
         window.location = 'https://word.dailybrainplay.com/'
     } else {
-
+        $('.word-play').show();
     }
 })
+
+$('.getphrazy-play').click(function () {
+    window.location = 'https://getphrazy.com/'
+})
+
+$('.word-play').click(function () {
+    window.location = 'https://word.dailybrainplay.com/'
+})  
