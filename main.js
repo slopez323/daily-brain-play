f926c79f053ab2051bba89e757905a86b1c90a9b
Sarah Lopez <slopez323@gmail.com>
2022-03-12 14:37:28 -0500

    new mobile layout

--- main.js
+++ main.js
@@ -4,30 +4,42 @@ function isTouchScreendevice() {
     return 'ontouchstart' in window || navigator.maxTouchPoints;
 };
 
-$('.getphrazy-container').click(function () {
-    // if (!isTouchScreendevice()) {
+if (isTouchScreendevice()) {
+    $('.back').hide()
+    $('.playnow').hide()
+    $('.up').hide()
+
+    $('#getphrazy-button').click(function () {
+        $('#getphrazy-desc').slideToggle();
+        $('.getphrazy-play').slideToggle();
+        $('.word-container').slideToggle();
+        $('.show').toggle();
+    })
+    $('#word-button').click(function () {
+        $('#word-desc').slideToggle();
+        $('.word-play').slideToggle();
+        $('.getphrazy-container').slideToggle();
+        $('.show').toggle();
+    })
+
+    $('#getphrazy-desc').click(function () {
         window.location = 'https://getphrazy.com/'
-    // } else if (window.width <= 540) {
-    //     $('.getphrazy-play').slideDown();
-    // } else {
-    //     $('.getphrazy-play').show();
-    // }
-})
-
-$('.word-container').click(function () {
-    // if (!isTouchScreendevice()) {
+    })
+    $('.getphrazy-play').click(function () {
+        window.location = 'https://getphrazy.com/'
+    })
+    $('#word-desc').click(function () {
         window.location = 'https://word.dailybrainplay.com/'
-    // } else if (window.width <= 540) {
-    //     $('.word-play').slideDown();
-    // } else {
-    //     $('.word-play').show();
-    // }
-})
+    })
+    $('.word-play').click(function () {
+        window.location = 'https://word.dailybrainplay.com/'
+    })
+} else {
+    $('.getphrazy-container').click(function () {
+        window.location = 'https://getphrazy.com/'
+    })
 
-// $('.getphrazy-play').click(function () {
-//     window.location = 'https://getphrazy.com/'
-// })
-
-// $('.word-play').click(function () {
-//     window.location = 'https://word.dailybrainplay.com/'
-// })  
+    $('.word-container').click(function () {
+        window.location = 'https://word.dailybrainplay.com/'
+    })
+}
