5a029b337e8b1b15d0c29f865d58750b664e706c
Sarah Lopez <slopez323@gmail.com>
2022-03-12 14:51:35 -0500

    diff portrait and landscape

--- main.js
+++ main.js
@@ -4,24 +4,42 @@ function isTouchScreendevice() {
     return 'ontouchstart' in window || navigator.maxTouchPoints;
 };
 
+$(document).ready(setHandlers);
+$(window).resize(setHandlers);
+
+function setHandlers(){
 if (isTouchScreendevice()) {
     $('.back').hide()
     $('.playnow').hide()
     $('.up').hide()
 
-    $('#getphrazy-button').click(function () {
-        $('#getphrazy-desc').slideToggle();
-        $('.getphrazy-play').slideToggle();
-        $('.word-container').slideToggle();
-        $('.show').toggle();
-    })
-    $('#word-button').click(function () {
-        $('#word-desc').slideToggle();
-        $('.word-play').slideToggle();
-        $('.getphrazy-container').slideToggle();
-        $('.show').toggle();
-    })
-
+    if ($(window).width() > 540) {
+        $('#getphrazy-button').click(function () {
+            $('#getphrazy-desc').slideToggle();
+            $('.getphrazy-play').slideToggle();
+            $('.word-container').slideToggle();
+            $('.show').toggle();
+        })
+        $('#word-button').click(function () {
+            $('#word-desc').slideToggle();
+            $('.word-play').slideToggle();
+            $('.getphrazy-container').slideToggle();
+            $('.show').toggle();
+        })
+    } else {
+        $('#getphrazy-button').click(function () {
+            $('#getphrazy-desc').slideToggle();
+            $('.getphrazy-play').slideToggle();
+            // $('.word-container').slideToggle();
+            $('#getphrazy-button .show').toggle();
+        })
+        $('#word-button').click(function () {
+            $('#word-desc').slideToggle();
+            $('.word-play').slideToggle();
+            // $('.getphrazy-container').slideToggle();
+            $('#word-button .show').toggle();
+        })
+    }
     $('#getphrazy-desc').click(function () {
         window.location = 'https://getphrazy.com/'
     })
@@ -42,4 +60,5 @@ if (isTouchScreendevice()) {
     $('.word-container').click(function () {
         window.location = 'https://word.dailybrainplay.com/'
     })
+}
 }
