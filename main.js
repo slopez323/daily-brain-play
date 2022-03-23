2362203606caa7a44b75ea436db55626b3fd5791
Sarah Lopez <slopez323@gmail.com>
2022-03-23 18:16:08 -0400

    updated layout

--- main.js
+++ main.js
@@ -1,65 +1,19 @@
 const analytics = {
     PHRAZY: "clicked_getphrazy",
     WORD: "clicked_word"
-};
-
-$(document).ready(setHandlers);
-$(window).resize(setHandlers);
-
-function setHandlers() {
-    $('.flip-container').show();
-    $('.back').hide();
-    $('.playnow').hide();
-    $('.show').addClass('down');
-
-    $('#getphrazy-button').off();
-    $('#word-button').off();
-
-    if ($(window).width() > 767) {
-        $('#getphrazy-button').click(function () {
-            $('#getphrazy-desc').slideToggle();
-            $('.getphrazy-play').slideToggle();
-            $('.word-container').slideToggle();
-            $('#getphrazy-button .show').toggleClass('down');
-        });
-        $('#word-button').click(function () {
-            $('#word-desc').slideToggle();
-            $('.word-play').slideToggle();
-            $('.getphrazy-container').slideToggle();
-            $('#word-button .show').toggleClass('down');
-        });
-    } else {
-        $('#getphrazy-button').click(function () {
-            $('#getphrazy-desc').slideToggle();
-            $('.getphrazy-play').slideToggle();
-            $('#getphrazy-button .show').toggleClass('down');
-        });
-        $('#word-button').click(function () {
-            $('#word-desc').slideToggle();
-            $('.word-play').slideToggle();
-            $('#word-button .show').toggleClass('down');
-        });
-    };
-    $('#getphrazy-desc').click(function () {
-        sendEvent(analytics.PHRAZY);
-        window.location = 'https://getphrazy.dailybrainplay.com/'
-    });
-    $('.getphrazy-play').click(function () {
-        sendEvent(analytics.PHRAZY);
-        window.location = 'https://getphrazy.dailybrainplay.com/'
-    });
-    $('#word-desc').click(function () {
-        sendEvent(analytics.WORD);
-        window.location = 'https://word.dailybrainplay.com/'
-    });
-    $('.word-play').click(function () {
-        sendEvent(analytics.WORD);
-        window.location = 'https://word.dailybrainplay.com/'
-    });
-};
-
-function sendEvent(action, values) {
+  };
+  
+  $('.getphrazy-container').click(function () {
+    sendEvent(analytics.PHRAZY);
+    window.location = 'https://getphrazy.dailybrainplay.com/'
+  });
+  $('.word-container').click(function () {
+    sendEvent(analytics.WORD);
+    window.location = 'https://word.dailybrainplay.com/'
+  });
+  
+  function sendEvent(action, values) {
     if (window.gtag) {
         gtag("event", action, { data: JSON.stringify(values) });
     };
-};
+  };
